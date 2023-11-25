5e43be0cdc1b23554b7b3f195361455b8f45a38f
MinhLe <aatrox0912@gmail.com>
2023-11-26 01:01:26 +0700

    kanban board style

---
+++ src/views/tims/task/mockdata.js
@@ -0,0 +1,118 @@
+export const newTasksData = [
+    {
+        "id": "64a3db3471c39b1accf9e156",
+        "title": "[CP]/[Đơn hàng] Thêm chức năng gia hạn/nâng cấp cho các đơn hàng",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Mai Long Vũ" },
+        "code": 1812,
+        "type": "task",
+    },
+    {
+        "id": "6433883971c39b2008af0bd6",
+        "title": "[CP] Thêm trường Note trên mục Bản in ",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Hòa" },
+        "code": 1665,
+        "type": "task",
+    }
+];
+export const processTasksData = [
+    {
+        "id": "652cba9b71c39b17a834e5bb",
+        "title": "Chuyển thông tin thành viên từ vietnamgiapha cho dòng họ Vũ Tộc",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1874,
+        "type": "task",
+    },
+    {
+        "id": "637f2e9771c39b2a20e49cfe",
+        "title": "[API] Thêm API cho phép sửa trường trong_ho",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1355,
+        "type": "task",
+    }
+];
+export const doingTasksData = [
+    {
+        "id": "632960ee6e38501e0840684f",
+        "title": "Làm bài ngày 22/09",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Phan Thị Ngọc Minh" },
+        "code": 1146,
+        "type": "task",
+    },
+    {
+        "id": "6226c0e96e385014f8c7dcaa",
+        "title": "Viết bài \"Ra mắt ứng dụng Việt Tộc trên nền tảng Android\"",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Phan Thị Ngọc Minh" },
+        "code": 647,
+        "type": "task",
+    }
+];
+export const doneTasksData = [
+    {
+        "id": "65000dd971c39b0b4891048e",
+        "title": "Lấy thông tin dòng họ Phan - Phú Yên từ trang vietnamgiapha phục vụ import cây Việt Tộc",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1863,
+        "type": "task",
+    },
+    {
+        "id": "654def5c71c39b1da0762a6b",
+        "title": "Chuyển thông tin dòng họ Trần Đắc từ giaphavietnam phục vụ import cây Việt Tộc",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1881,
+        "type": "task",
+    },
+    {
+        "id": "654068f271c39b1f28ec76bd",
+        "title": "Chuyển thông tin thành viên từ giaphavietnam cho dòng họ Nguyễn Văn",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1876,
+        "type": "task",
+    },
+    {
+        "id": "6502708b71c39b1e602fea54",
+        "title": "Lấy thông tin dòng họ Phạm - Ninh Bình từ trang vietnamgiapha phục vụ import cây Việt Tộc",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1865,
+        "type": "task",
+    }
+];
+export const blockingTasksData = [
+    {
+        "id": "6502708b71c39b1e602fea54",
+        "title": "Lấy thông tin dòng họ Phạm - Ninh Bình từ trang vietnamgiapha phục vụ import cây Việt Tộc",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1865,
+        "type": "task",
+    }
+];
+export const verifyingTasksData = [
+    {
+        "id": "6502708b71c39b1e602fea54",
+        "title": "Lấy thông tin dòng họ Phạm - Ninh Bình từ trang vietnamgiapha phục vụ import cây Việt Tộc",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Nguyễn Duy Thịnh" },
+        "code": 1865,
+        "type": "task",
+    }
+];
+export const cancelTasksData = [
+    {
+        "id": "64b1081271c39b3710e43d24",
+        "title": "đăng bài zalo 18/7",
+        "priorityId": { "name": "", "color": "" },
+        "ownerId": { "name": "Trần Hoàng Việt" },
+        "code": 1839,
+        "type": "task",
+    }
+];
