1ff941f7d0424a6a0029db6b78dc484bf59df61e
MinhLe <aatrox0912@gmail.com>
2023-11-28 22:48:45 +0700

    Form báo cáo ngày

--- src/views/tims/workToday/workMember.vue
+++ src/views/tims/workToday/workMember.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, defineProps, ref } from 'vue';
 import account from "@/assets/images/account.jpg";
+import moment from 'moment';
 
 const props = defineProps({
     check: Boolean,
@@ -8,10 +9,52 @@ const props = defineProps({
 
 const listSprint = ref(['1', '2']);
 const listTasks = ref(['3', '4']);
+const openReportDrawer = ref(false);
+
+const input = ref({});
+const formRef = ref();
+const fileList = ref([]);
+const rules = ref({
+    title: [
+        { required: true, message: 'Bắt buộc nhập tên việc', trigger: 'change' },
+    ],
+    priority: [
+        { required: true, message: 'Bắt buộc chọn mức độ ưu tiên', trigger: 'change' },
+    ],
+    startDate: [
+        { type: 'date', required: true, message: 'Bắt buộc chọn ngày bắt đầu', trigger: 'change' }
+    ],
+    deadline: [
+        { type: 'date', message: 'Bắt buộc chọn deadline', trigger: 'change' }
+    ],
+    owner_id: [
+        { required: true, message: 'Bắt buộc chọn người làm', trigger: 'change' },
+    ],
+    verifier_id: [
+        { required: true, message: 'Bắt buộc chọn người duyệt', trigger: 'change' },
+    ],
+});
+const employees = ref([
+    { id: 1, email: 'thuntm@gmail.com', name: 'thuntm' },
+    { id: 2, email: 'minhlq@gmail.com', name: 'minhlq' },
+]);
 
 onMounted(() => {
     console.log(props)
 })
+
+const report = () => {
+    openReportDrawer.value = true;
+}
+
+const onSubmit = () => {
+    formRef.value.validate((formValidated, fields) => {
+        console.log('form validated', input.value);
+        console.log('form validated', formValidated);
+        console.log('form validated', fields);
+    })
+};
+
 </script>
 
 <template>
@@ -89,11 +132,11 @@ onMounted(() => {
             </div>
             <div class="member-work-footer">
                 <div class="footer-left">
-                    <span>Báo cáo</span>
+                    <button class="btn" @click="report">Báo cáo</button>
                 </div>
                 <div class="footer-right">
-                    <span class="m-r-10">Hỏi</span>
-                    <span>Giao việc</span>
+                    <button class="btn m-r-10">Hỏi</button>
+                    <button class="btn">Giao việc</button>
                 </div>
             </div>
         </div>
@@ -111,4 +154,47 @@ onMounted(() => {
             </div>
         </div>
     </div>
+
+    <el-drawer v-model="openReportDrawer" direction="rtl" size="40%">
+        <template #header="{ titleId, titleClass }">
+            <div :id="titleId" :class="titleClass">
+                Báo cáo ngày {{ moment(new Date()).format("DD/MM/YYYY") }}
+            </div>
+        </template>
+        <el-form label-position="top" require-asterisk-position="right" :model="input" :rules="rules" ref="formRef">
+            <el-form-item class="mb-4" label="Người nhận" prop="emailSend">
+                <el-input v-model="input.emailSend" placeholder="Người nhận" />
+            </el-form-item>
+            <el-form-item class="mb-4" label="CC cho" prop="emailCC">
+                <el-select v-model="input.emailCC" multiple placeholder="CC cho" class="w-100">
+                    <el-option 
+                        v-for="item in employees" 
+                        :key="item.id" 
+                        :label="item.name"
+                        :value="item.email"
+                    >
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item class="mb-4" label="Tiêu đề" prop="title">
+                <el-input v-model="input.title" placeholder="Tiêu đề" />
+            </el-form-item>
+            <el-form-item class="mb-4" label="Nội dung" prop="content">
+                <el-input type="textarea" rows="10" v-model="input.content" placeholder="Nội dung" />
+            </el-form-item>
+            <el-upload
+                action=""
+                multiple
+                :limit="3"
+                :file-list="fileList"
+                :auto-upload="false"
+            >
+                <el-button size="small" type="primary">File đính kèm</el-button>
+            </el-upload>
+            <i class="text-muted">Báo cáo này sẽ tự động gửi vào hòm thư và email của trưởng nhóm</i>
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-success w-25" type="button" @click="onSubmit">Lưu</button>
+            </div>
+        </el-form>
+    </el-drawer>
 </template>
